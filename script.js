c9ea0593a19d6e83b37d5740f43a0e1c706dbbbd
Armin Rahbar <arminmuch41@gmail.com>
2024-10-27 10:26:12 +0330

    GitHub Pages Initialized

---
+++ script.js
@@ -0,0 +1,10 @@
+const btn = document.getElementById("btn");
+const text = document.getElementById("text");
+
+btn.addEventListener("click", () => {
+  if (text.className === "hidden") {
+    text.className = "visible";
+  } else {
+    text.className = "hidden";
+  }
+});
